37f2a86eaa629be4dfc3eb30af8cc28a6c95270a
azran4u <azran4u@gmail.com>
2017-10-31 16:15:10 +0300

    change color

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function(socket){
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
+	console.log('a user connected');
+	socket.on('chat message', function(name, msg){
+
+		// check if we got command
+		if( msg.startsWith("/") ){
+			console.log("user entered command");
+			socket.emit('change style', msg);
+		}
+		else{
+			console.log('user sent message');
+			socket.broadcast.emit('chat message',name, msg);
+		}
+
+
+	});
+	socket.on('change style', function(style){
+		var format = 'Style: ' + style;
+
+		// print the message to log
+		console.log(format);
 	});
 });
 
