3c720ecba996422146b63757d19e96cc837d9a4d
azran4u <azran4u@gmail.com>
2017-11-01 10:58:17 +0200

    fixed bug socket.io

--- index.js
+++ index.js
@@ -3,32 +3,24 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+// in case user entered URL
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function(socket){
 	console.log('a user connected');
-	socket.on('chat message', function(name, msg){
+    socket.on('chat message', function (name, msg) {
 
-		// check if we got command
-		if( msg.startsWith("/") ){
-			console.log("user entered command");
-			socket.emit('change style', msg);
-		}
-		else{
-			console.log('user sent message');
-			socket.broadcast.emit('chat message',name, msg);
-		}
-
-
-	});
-	socket.on('change style', function(style){
-		var format = 'Style: ' + style;
-
-		// print the message to log
-		console.log(format);
-	});
+        // check if we got command
+        if (msg.startsWith("/")) {
+            console.log("user entered command");
+        }
+        else {
+            console.log('user sent message');
+            socket.broadcast.emit('chat message', name, msg);
+        }
+    });
 });
 
 http.listen(port, function(){
